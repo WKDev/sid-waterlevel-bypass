ec473c87da37562e0b1ed3230caff6a83385f77f
WKDev <chanhyukman@gmail.com>
2022-07-06 11:01:05 +0900

    commit_on_main

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,26 @@
 import express, { Request, Response, NextFunction } from 'express';
+import mqtt, { IClientOptions } from 'mqtt';
+import 'dotenv/config'
+
+const options : IClientOptions = {
+  host: process.env.MQTT_HOST?.toString(),
+  port: Number(process.env.MQTT_PORT),
+  protocol: "mqtts",
+  username: String(process.env.MQTT_USER_ID),
+  password: String(process.env.MQTT_PASSWORD),
+};
 
 const app = express();
+const client = mqtt.connect(options)
 const port = process.env.PORT
+
+client.on("connect", () => {	
+  console.log("connected"+ client.connected);
+})
+
+client.on("error", (error) => { 
+  console.log("Can't connect" + error);
+})
 
 app.get('/welcome', (req: Request, res: Response, next: NextFunction) => {
     console.log(req)
